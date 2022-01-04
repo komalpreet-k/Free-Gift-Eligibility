664ea4f451c6dc766e58a4fdb8f2809eedd6ffe4
komalpreet103 <74217521+komalpreet103@users.noreply.github.com>
2022-01-04 12:55:22 -0600

    Modified code

--- Contest.java
+++ Contest.java
@@ -9,7 +9,17 @@ public class Contest {
             System.out.println("Great");
         } else {
             System.out.println("Would you like to join us for membership?");
+            userAnswer = kb.next();
+            if (userAnswer.equalsIgnoreCase("Yes")) {
+                System.out.println("Great! Here is your new id");
+            }
         }
+        if (userAnswer.equalsIgnoreCase("Yes")) {
+            System.out.println("I am glad, you are ligible for free gift in Store!");
+        } else {
+            System.out.println("I am sorry! only store members are eligible for free gift.");
+        }
+
         kb.close();
 
     }
