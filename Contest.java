ef23d4b3a001b9acbb39240ed1530e90ad5e071f
komalpreet103 <74217521+komalpreet103@users.noreply.github.com>
2022-01-08 17:52:57 -0600

    more features

--- Contest.java
+++ Contest.java
@@ -5,17 +5,23 @@ public class Contest {
         System.out.println("Do you have membership with our Store?");
         Scanner kb = new Scanner(System.in);
         String userAnswer = kb.next();
+        boolean eligibility = false;
         if (userAnswer.equalsIgnoreCase("Yes")) {
             System.out.println("Great");
+            eligibility = true;
         } else {
             System.out.println("Would you like to join us for membership?");
             userAnswer = kb.next();
             if (userAnswer.equalsIgnoreCase("Yes")) {
                 System.out.println("Great! Here is your new id");
+                eligibility = true;
+            } else {
+                System.out.println("It is free of cost, let us know in future if you want to sign up");
+                eligibility = false;
             }
         }
-        if (userAnswer.equalsIgnoreCase("Yes")) {
-            System.out.println("I am glad, you are ligible for free gift in Store!");
+        if (eligibility) {
+            System.out.println("I am glad, you are eligible for free gift in Store!");
         } else {
             System.out.println("I am sorry! only store members are eligible for free gift.");
         }
